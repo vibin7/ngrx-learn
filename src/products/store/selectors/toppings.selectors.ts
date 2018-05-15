1bc246b85b2a6b259c83a16519b0b473554517c9
vibin7 <vibinthomas777@gmail.com>
2018-05-15 15:04:31 +0530

    Add UI feature in toppings

--- src/products/store/selectors/toppings.selectors.ts
+++ src/products/store/selectors/toppings.selectors.ts
@@ -10,6 +10,10 @@ export const getToppingsState = createSelector(fromFeature.getProductsState,
 
 export const getToppingEntities = createSelector(getToppingsState,fromToppings.getToppingEntities);
 
+export const getSelectedToppings = createSelector(
+  getToppingsState,
+  fromToppings.getSelectedToppings
+)
 export const getAllToppings = createSelector( getToppingEntities, entities => {
   return Object.keys(entities).map( id => entities[parseInt(id, 10)]);
 })
